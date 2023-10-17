fa1d983875fedc02d59a6da82e0795bd2bc318c5
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2023-10-17 07:01:36 -0500

    Add files via upload

---
+++ lambda_handler.py
@@ -0,0 +1,85 @@
+import json
+import csv
+import boto3
+
+# Initialize AWS services
+s3_client = boto3.client('s3')
+connect_client = boto3.client('connect')
+
+instance_id =  "1a60da9e-2e91-47ec-980a-29b437893c21"
+routing_profile_id = "9f97c09b-1acd-4517-9084-be028242e392"
+
+
+def lambda_handler(event, context):
+    # Get the S3 bucket and object key from the S3 event
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    s3_key = event['Records'][0]['s3']['object']['key']
+    
+    # Download the CSV file from S3
+    response = s3_client.get_object(Bucket=s3_bucket, Key=s3_key)
+    csv_data = response['Body'].read().decode('utf-8').splitlines()
+
+    for row in csv.reader(csv_data):
+        action, username, password, routing_profile_id, instance_id, user_id = row
+
+        if action == 'create':
+            create_connect_user(username, password, routing_profile_id, instance_id)
+        elif action == 'delete':
+            delete_connect_user(user_id, instance_id)
+        else:
+            print(f"Invalid action: {action}")
+
+def create_connect_user(username, password, routing_profile_id=routing_profile_id, instance_id=instance_id):
+    try:
+        response = connect_client.create_user(
+            Username=username,
+            Password=password,
+            IdentityInfo={
+                'FirstName': username,  
+                'LastName': 'User',    
+            },
+            PhoneConfig={
+                'PhoneType': 'SOFT_PHONE',
+            },
+            SecurityProfileIds=[routing_profile_id],
+            RoutingProfileId=routing_profile_id,
+            InstanceId=instance_id
+        )
+        print(f"User {username} created with ID: {response['UserId']}")
+    except Exception as e:
+        print(f"Error creating user: {str(e)}")
+
+def delete_connect_user(user_id, instance_id):
+    try:
+        response = connect_client.delete_user(
+            InstanceId=instance_id,
+            UserId=user_id
+        )
+        print(f"User {user_id} deleted successfully.")
+    except Exception as e:
+        print(f"Error deleting user: {str(e)}")
+
+
+#creating and deleting a user requires corresponding ID key for the resource 
+#aws connect list-instances
+#aws connect list-users --instance_id
+
+# create_connect_user("jubei","Passw0rd!",routing_profile_id, instance_id)
+# delete_connect_user(user_id="cbefd263-154f-4ee8-ade2-85cc2d2d79ad",instance_id=instance_id)
+
+#format of csv
+# Action,Username,Password,RoutingProfileId,InstanceId
+# create,user1,password123,profile-id-1,instance-id-1,
+# create,user2,password456,profile-id-2,instance-id-1,
+# delete,user3,,profile-id-3,instance-id-1
+#  aws connect list-users --instance-id "1a60da9e-2e91-47ec-980a-29b437893c21"
+
+# {
+#     "UserSummaryList": [
+#         {
+#             "Id": "481e35d8-68a1-4f0b-898d-fe88ea549179",
+#             "Arn": "arn:aws:connect:us-east-1:3325:instance/1/agent/481e35d8-fe88ea549179",
+#             "Username": "thecloudshepherd"
+#         }
+#     ]
+# }
---
+++ main.tf
@@ -0,0 +1,100 @@
+# Define the AWS provider
+
+variable "bucket" {
+  default = "the-cloud-shepherd-csv-linkedin"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Define the S3 bucket for storing the CSV files
+resource "aws_s3_bucket" "csv_bucket" {
+  bucket = var.bucket
+}
+
+# # Define the Lambda function resource
+resource "aws_lambda_function" "csv_to_json" {
+  function_name = "csv-to-json"
+  filename = "lambda_function_payload.zip"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  handler = "lambda_handler.lambda_handler"
+  role = aws_iam_role.lambda.arn
+  runtime = "python3.9"
+}
+
+# Define the IAM role for the Lambda function
+resource "aws_iam_role" "lambda" {
+  name = "lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Define the IAM policy for the Lambda function
+resource "aws_iam_policy" "lambda" {
+  name = "lambda-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# # Attach the IAM policy to the IAM role
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  policy_arn = aws_iam_policy.lambda.arn
+  role = aws_iam_role.lambda.name
+}
+
+# Define the S3 bucket notification configuration
+resource "aws_s3_bucket_notification" "csv_to_json" {
+    depends_on = [ aws_lambda_permission.allow_bucket ]
+    bucket = aws_s3_bucket.csv_bucket.id
+#   bucket = var.bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.csv_to_json.arn
+    events              = ["s3:ObjectCreated:*"]
+    # filter_prefix       = "csv/"
+    filter_suffix       = ".csv"
+  }
+
+  
+}
+
+#this is required r
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.csv_to_json.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.csv_bucket.arn
+}
+
+
+#You will get the following error (below) if you do not create the "aws_lambda_permission" "allow_bucket" #resource
+
+# │ Error: creating S3 Bucket (the-cloud-shepherd-csv-linkedin) Notification: operation error S3: 
+# PutBucketNotificationConfiguration, https response error StatusCode:
+#  400, RequestID: 1A4BC98NYG2308CR, HostID: TxPJRuOlSiXoWQ0Q5VODHZbFCJ5re41YLEqhgB7cFqWPqadM5c3Rop3713dYajmg5x4C+zIFNJ0=,
+#  api error InvalidArgument: Unable to validate the following destination configurations
