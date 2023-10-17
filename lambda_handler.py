839a866d420b4c2f85442b543cf5aa0d89945ef6
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2023-10-17 11:19:24 -0500

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -6,8 +6,8 @@ import boto3
 s3_client = boto3.client('s3')
 connect_client = boto3.client('connect')
 
-# instance_id =  "1a60da9e-2e91-47ec-980a-29b437893c21"
-# routing_profile_id = "9f97c09b-1acd-4517-9084-be028242e392"
+instance_id =  "1a60da9e-2e91-47ec-980a-29b437893c21"
+routing_profile_id = "9f97c09b-1acd-4517-9084-be028242e392"
 
 
 def lambda_handler(event, context):
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
         else:
             print(f"Invalid action: {action}")
 
-def create_connect_user(username, password, routing_profile_id, instance_id):
+def create_connect_user(username, password, routing_profile_id=routing_profile_id, instance_id=instance_id):
     try:
         response = connect_client.create_user(
             Username=username,
@@ -76,22 +76,15 @@ def delete_connect_user(user_id, instance_id):
 #aws connect list-instances
 #aws connect list-users --instance_id
 
-# create_connect_user("jubei","Passw0rd!",routing_profile_id, instance_id)
-# delete_connect_user(user_id="cbefd263-154f-4ee8-ade2-85cc2d2d79ad",instance_id=instance_id)
 
-#format of csv
+#format of csv for adding users
 # Action,Username,Password,RoutingProfileId,InstanceId
 # create,user1,password123,profile-id-1,instance-id-1,
 # create,user2,password456,profile-id-2,instance-id-1,
-# delete,user3,,profile-id-3,instance-id-1
-#  aws connect list-users --instance-id "1a60da9e-2e91-47ec-980a-29b437893c21"
 
-# {
-#     "UserSummaryList": [
-#         {
-#             "Id": "481e35d8-68a1-4f0b-898d-fe88ea549179",
-#             "Arn": "arn:aws:connect:us-east-1:3325:instance/1/agent/481e35d8-fe88ea549179",
-#             "Username": "thecloudshepherd"
-#         }
-#     ]
-# }
+
+#format of csv for deleting users
+# Action,UserId,InstanceId
+# delete,"09542ed4-1eab-4151-b4be-6856a41724ab","1a60da9e-2e91-47ec-980a-29b437893c21",
+# delete, "17db7473-1a7a-48b4-b675-e62e50e12e0b", "1a60da9e-2e91-47ec-980a-29b437893c21"
+
